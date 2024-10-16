ae578ffadd89d329c7069157094231b2bc790253
Moultrie Dangerfield <moultriedanger@gmail.com>
2024-10-16 17:53:01 -0400

    All the backend works. Need to create function to display data

--- app/app.py
+++ app/app.py
@@ -1,17 +1,60 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
+import sys
+import logging
 
 app = Flask(__name__)
 cors = CORS(app)
 
 @app.route("/2023songs")
-def csc_342_groups():
-       
+def search_songs():
+
+    filtered = []
+
+    searchBy = request.args.get('searchBy', default='title')
+    sortBy = request.args.get('sortBy', default='popularity')
+    includeExplicit = request.args.get('checkbox', default='true')
+    songSearch = request.args.get('songSearch', default='')
+
+    #handle a null value
+    if songSearch == '':
+        return filtered
+
     with open('../top_tracks_2023.json', 'r') as file:
-        data2023 = json.load(file)  # Load JSON data into a Python object
-  
-    return jsonify(data2023)  # Return the JSON response
+        data2023 = json.load(file)
+
+    #Filter data
+    for song in data2023:
+        if  includeExplicit == 'true':
+            field = song[searchBy]
+            if type(field) == list:
+                for s in field:
+                    if songSearch in s.lower():
+                        filtered.append(song)
+                    else: 
+                        continue
+            else:
+                if songSearch in field.lower():
+                    filtered.append(song)
+        elif includeExplicit == 'false':
+            #filter for non explicit songs
+            if not song['explicit']:
+                field = song[searchBy]
+                if type(field) == list:
+                    for s in field:
+                        if songSearch in s.lower():
+                            filtered.append(song)
+                        else: 
+                            continue
+                else:
+                    if songSearch in field.lower():
+                        filtered.append(song)
+ 
+    return filtered
+
+def duration_sort():
+    pass
 
 
 if __name__ == '__main__':
--- script.js
+++ script.js
@@ -1,20 +1,43 @@
-function search(){
-    // value from dropdown1
-    const searchBy = document.getElementById("search-by");
-    const searchValue = searchBy.value;
+const xhr = new XMLHttpRequest();
 
-    //dropdown2
-    const sortBy = document.getElementById("sort-by");
-    const sortValue = sortBy.value;
+let songs = null;
 
-    const checkbox = document.getElementById('checkbox');
-    const checkValue = checkbox.checked;
+//retrieve search paramaters
+async function search(){
+  // value from dropdown1
+  const searchBy = document.getElementById("search-by").value;
+ 
+  const sortBy = document.getElementById("sort-by").value;
+ 
+  const checkbox = document.getElementById('checkbox').checked;
+  
+  const songSearch = document.getElementById('songSearch').value;
 
-    const songSearch = document.getElementById('songSearch');
-    const songName = songSearch.value;
+  //Request  
+  let url = `http://127.0.0.1:5000/2023songs?searchBy=${encodeURIComponent(searchBy)}&sortBy=${encodeURIComponent(sortBy)}&checkbox=${encodeURIComponent(checkbox)}&songSearch=${encodeURIComponent(songSearch)}`;
+  let prom = await fetch(url);
+  let result = await prom.json();
+  songs = result;
 
+  sortSongs(songs, sortBy);
+}
 
-    
+//sort the songs
+function sortSongs(songs, status){
 
+  if (status == 'popularity'){
+    sorted_songs = songs.sort((a, b) => b.popularity - a.popularity);
+  } 
+  else if(status == 'duration'){
+    sorted_songs = songs.sort((a, b) => b.duration_mins - a.duration_mins);
+  }
 
+  //call display funciton
+  display_songs(sorted_songs)
+}
+
+function display_songs(songs){
+  if (songs.length == 0){
+    console.log("empty")
+  }
 }
